b79ce2623ee098b7043099d02580a56a40ffee8b
srinivasaramanujam315 <srinivasaramanujam.ec24@builderscollege.edu.in>
2026-02-04 14:30:01 +0530

    Add files via upload

---
+++ insert - Copy.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Node* insertAtBeginning(Node* head, int value) {
+    Node* newNode = createNode(value);
+    newNode->next = head;
+    return newNode;
+}
+
+void displayList(Node* head) {
+    printf("Linked List: ");
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+int main() {
+    Node* head = NULL;
+    
+    head = insertAtBeginning(head, 10);
+    head = insertAtBeginning(head, 20);
+    head = insertAtBeginning(head, 30);
+    head = insertAtBeginning(head, 40);
+    
+    displayList(head);
+    
+    freeList(head);
+    return 0;
+}
+
